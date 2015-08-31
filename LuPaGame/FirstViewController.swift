a97eb8240819a69a7236a71585e3f05c24a8e968
Fernando Gómez <gomezhyuuga@gmail.com>
2015-08-30 23:55:47 -0500

    Initial Commit

---
+++ LuPaGame/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  LuPaGame
+//
+//  Created by Fernando Gómez Herrera on 8/30/15.
+//  Copyright (c) 2015 Fernando Gómez. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ LuPaGame/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  LuPaGame
+//
+//  Created by Fernando Gómez Herrera on 8/30/15.
+//  Copyright (c) 2015 Fernando Gómez. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
